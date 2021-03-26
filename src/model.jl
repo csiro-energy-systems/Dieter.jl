c0de690ea5413a12c32aba39959bbfd113d7dcd2
James <James.Foster@csiro.au>
2021-03-26 18:11:34 +1100

    ArcWeight name sync.

--- src/model.jl
+++ src/model.jl
@@ -148,7 +148,7 @@ function build_model!(dtr::DieterModel)
     InvestmentCostTransExp = filter(x -> !ismissing(x[2]) && x[1] in Arcs, dtr.parameters[:InvestmentCostTransExp]) # Units:  currency/MW; ; REZ capacity expansion costs, amortised to year
     InvestmentCostREZ_Exp  = filter(x -> !ismissing(x[2]) && x[1] in Arcs_REZones, dtr.parameters[:InvestmentCostTransExp]) # Units:  currency/MW; ; Transmission interconnector capacity expansion costs, amortised to year
 
-    ArcWeight = dtr.parameters[:arc_weight]
+    ArcWeight = dtr.parameters[:ArcWeight]
 
     Load = dtr.parameters[:Load] # Units: MWh per time-interval; wholesale energy demand within a time-interval (e.g. hourly or 1/2-hourly)
     NegOpDemand = dtr.parameters[:NegOpDemand] # Units: MWh per time-interval; wholesale energy produced from behind-the-meter generation (e.g. PV or EV)
